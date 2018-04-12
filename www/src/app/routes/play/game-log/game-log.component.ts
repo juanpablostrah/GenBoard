392321cdbfc91a9a39867079528e07312ddc2aa0
Juan Pablo Strah <strahjuanpablo@gmail.com>
2018-04-12 20:57:21 -0300

    add game log component view

--- www/src/app/routes/play/game-log/game-log.component.ts
+++ www/src/app/routes/play/game-log/game-log.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Output } from '@angular/core';
+import { Input } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-game-log',
@@ -7,7 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameLogComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  dataSet: [{}]
+
+  @Output()
+  onRoll: EventEmitter<void>
+
+  constructor() {
+    this.onRoll = new EventEmitter();
+  }
+
+  doLog(dataSet: [any]){
+    
+  }
 
   ngOnInit() {
   }
