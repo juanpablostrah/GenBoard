454dc7ad2e26b27177b184e73b2a80673bcf71f5
Juan Pablo Strah <strahjuanpablo@gmail.com>
2018-04-15 20:09:07 -0300

    prueba sin remover dados

--- www/src/app/routes/play/canvas-dice-roll/canvas-dice-roll.component.ts
+++ www/src/app/routes/play/canvas-dice-roll/canvas-dice-roll.component.ts
@@ -21,49 +21,79 @@ export class CanvasDiceRollComponent implements AfterViewInit {
 
   scene: any
   dice: any
+  world: any
+  camera: any
+  renderer: any
+  controls: any
+
   constructor() {
     this.dice = []
   }
 
-  doRoll(dataSet: [any]){
-    for (var i = 0; i < this.dice.length; i++) {
-        this.scene.remove(this.dice[i].die.getObject());
-    }
-    this.dice.length = 0;
+  createDice() {
+    var dataSet = [{
+      descriptor: 4,
+      value: 1
+    },{
+      descriptor: 6,
+      value: 1
+    },{
+      descriptor: 8,
+      value: 1
+    },{
+      descriptor: 10,
+      value: 1
+    },{
+      descriptor: 12,
+      value: 1
+    },{
+      descriptor: 20,
+      value: 1
+    }]
+
     var colors = ['#ff0000', '#ffff00', '#00ff00', '#0000ff', '#ff00ff', '#ff005f'];
 
     for (var i = 0; i < dataSet.length; i++) {
       var dataSetItem = dataSet[i];
       for (var j = 0; j < dataSetItem.value; j++) {
-
         switch(dataSetItem.descriptor ){
           case 4:
-              var die = new DiceD4({size: 1.5, backColor: colors[0]});
-              break;
+          var die = new DiceD4({size: 1.5, backColor: colors[0]});
+          break;
           case 6:
-              var die = new DiceD6({size: 1.5, backColor: colors[1]});
-              break;
+          var die = new DiceD6({size: 1.5, backColor: colors[1]});
+          break;
           case 8:
-              var die = new DiceD8({size: 1.5, backColor: colors[2]});
-              break;
+          var die = new DiceD8({size: 1.5, backColor: colors[2]});
+          break;
           case 10:
-              var die = new DiceD10({size: 1.5, backColor: colors[3]});
-              break;
+          var die = new DiceD10({size: 1.5, backColor: colors[3]});
+          break;
           case 12:
-              var die = new DiceD12({size: 1.5, backColor: colors[4]});
-              break;
+          var die = new DiceD12({size: 1.5, backColor: colors[4]});
+          break;
           case 20:
-              var die = new DiceD20({size: 1.5, backColor: colors[5]});
-              break;
+          var die = new DiceD20({size: 1.5, backColor: colors[5]});
+          break;
           default:
-              continue;
+          continue;
         }
         this.scene.add(die.getObject());
-        this.dice.push({die: die, value:dataSetItem.results[j]});
+        this.dice.push({die: die, value: 1});
 
       }
     }
+  }
 
+  doRoll(dataSet: [any]){
+    this.dice = dataSet.map( (each) => {
+      var sceneDie = this.dice.find((elem)=>{
+        return elem.die.values == each.descriptor
+      })
+      sceneDie.value = each.results[0]
+      return sceneDie
+    })
+    console.log(this.dice)
     var diceValues = [];
     for (var i = 0; i < this.dice.length; i++) {
       let diceI = this.dice[i].die;
@@ -78,49 +108,50 @@ export class CanvasDiceRollComponent implements AfterViewInit {
       let rand = Math.random() * 5;
       diceI.getObject().body.velocity.set(25 + rand, 40 + yRand, 15 + rand);
       diceI.getObject().body.angularVelocity.set(20 * Math.random() -10, 20 * Math.random() -10, 20 * Math.random() -10);
-      console.log(value, diceI)
       diceValues.push({dice: diceI, value: value});
     }
 
+    console.log(diceValues)
+
     DiceManager.prepareValues(diceValues);
+
 
   }
 
   ngAfterViewInit() {
-    requestAnimationFrame( animate );
-    var container, scene, camera, renderer, controls, world;
+    var container;
     var dice = this.dice;
     // SCENE
-  	this.scene = scene = new THREE.Scene();
+  	this.scene = new THREE.Scene();
   	// CAMERA
   	//var SCREEN_WIDTH = window.innerWidth, SCREEN_HEIGHT = window.innerHeight;
     var SCREEN_WIDTH = 500;
     var SCREEN_HEIGHT = 500;
   	var VIEW_ANGLE = 45, ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT, NEAR = 0.01, FAR = 20000;
-  	camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR);
-  	scene.add(camera);
-  	camera.position.set(0,30,30);
+  	this.camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR);
+  	this.scene.add(this.camera);
+  	this.camera.position.set(0,30,30);
   	// RENDERER
-    renderer = new THREE.WebGLRenderer( {antialias:true} );
-  	renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
-    renderer.shadowMap.enabled = true;
-    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    this.renderer = new THREE.WebGLRenderer( {antialias:true} );
+  	this.renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+    this.renderer.shadowMap.enabled = true;
+    this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
   	//container = document.getElementById( 'renderContainer' );
     container = this.renderContainerElem.nativeElement;
-  	container.appendChild( renderer.domElement );
+  	container.appendChild( this.renderer.domElement );
   	// EVENTS
   	// CONTROLS
-  	controls = new OrbitControls( camera, renderer.domElement );
+  	this.controls = new OrbitControls( this.camera, this.renderer.domElement );
 
   	let ambient = new THREE.AmbientLight('#ffffff', 0.1);
-  	scene.add(ambient);
+  	this.scene.add(ambient);
 
     let directionalLight = new THREE.DirectionalLight('#ffffff', 0.5);
     directionalLight.position.x = -1000;
     directionalLight.position.y = 1000;
     directionalLight.position.z = 1000;
-    scene.add(directionalLight);
+    this.scene.add(directionalLight);
 
     let light = new THREE.SpotLight(0xefdfd5, 0.5);
     light.position.y = 100;
@@ -130,7 +161,7 @@ export class CanvasDiceRollComponent implements AfterViewInit {
     // light.shadow.camera.far = 110;
     light.shadow.mapSize.width = 1024;
     light.shadow.mapSize.height = 1024;
-    scene.add(light);
+    this.scene.add(light);
 
 
   	// FLOOR
@@ -139,62 +170,57 @@ export class CanvasDiceRollComponent implements AfterViewInit {
   	var floor = new THREE.Mesh(floorGeometry, floorMaterial);
   	floor.receiveShadow  = true;
   	floor.rotation.x = Math.PI / 2;
-  	scene.add(floor);
+  	this.scene.add(floor);
   	// SKYBOX/FOG
   	var skyBoxGeometry = new THREE.CubeGeometry( 10000, 10000, 10000 );
   	var skyBoxMaterial = new THREE.MeshPhongMaterial( { color: 0x9999ff, side: THREE.BackSide } );
   	var skyBox = new THREE.Mesh( skyBoxGeometry, skyBoxMaterial );
   	//scene.add(skyBox);
-  	scene.fog = new THREE.FogExp2( 0x9999ff, 0.00025 );
+  	this.scene.fog = new THREE.FogExp2( 0x9999ff, 0.00025 );
 
   	////////////
   	// CUSTOM //
   	////////////
-    world = new CANNON.World();
+    this.world = new CANNON.World();
 
-    world.gravity.set(0, -9.82 * 20, 0);
-    world.broadphase = new CANNON.NaiveBroadphase();
-    world.solver.iterations = 16;
+    this.world.gravity.set(0, -9.82 * 20, 0);
+    this.world.broadphase = new CANNON.NaiveBroadphase();
+    this.world.solver.iterations = 16;
 
     window['THREE'] = THREE;
     window['CANNON'] = CANNON;
 
-    DiceManager.setWorld(world);
+    DiceManager.setWorld(this.world);
 
     //Floor
     let floorBody = new CANNON.Body({mass: 0, material: DiceManager.floorBodyMaterial});
     floorBody.addShape(new CANNON.Plane())
     floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2);
-    world.add(floorBody);
+    this.world.add(floorBody);
+    requestAnimationFrame( this.animate.bind(this) );
+    this.createDice();
+  }
 
-
-
-    //Walls
-
-    function animate() {
-      updatePhysics();
-    	render();
-    	update();
-      requestAnimationFrame( animate );
+  private updatePhysics() {
+    this.world.step(1.0 / 60.0);
+    for (var i in this.dice) {
+        this.dice[i].die.updateMeshFromBody();
     }
+  }
 
+  private update() {
+    this.controls.update();
+  }
 
-    function updatePhysics() {
-      world.step(1.0 / 60.0);
-      for (var i in dice) {
-          dice[i].die.updateMeshFromBody();
-      }
-    }
+  private render() {
+    this.renderer.render( this.scene, this.camera );
+  }
 
-    function update() {
-    	controls.update();
-    }
-
-    function render() {
-    	renderer.render( scene, camera );
-    }
-
-     requestAnimationFrame( animate );
+  public animate() {
+    this.updatePhysics();
+    this.render();
+    this.update();
+    requestAnimationFrame( this.animate.bind(this) );
   }
 
 }
