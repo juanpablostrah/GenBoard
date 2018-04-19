2a119257a654cf559abdb6695cd1a2d52ae93583
Pablo <loiaconopablo@gmail.com>
2018-04-19 15:22:16 -0300

    Textura piso y camara

--- www/src/app/routes/play/canvas-dice-roll/canvas-dice-roll.component.ts
+++ www/src/app/routes/play/canvas-dice-roll/canvas-dice-roll.component.ts
@@ -130,7 +130,8 @@ export class CanvasDiceRollComponent implements AfterViewInit {
   	var VIEW_ANGLE = 45, ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT, NEAR = 0.01, FAR = 20000;
   	this.camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR);
   	this.scene.add(this.camera);
-  	this.camera.position.set(0,30,30);
+	//para la posicion de la camara arriba de frente
+  	this.camera.position.set(0,180,30);
   	// RENDERER
     this.renderer = new THREE.WebGLRenderer( {antialias:true} );
   	this.renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
@@ -163,16 +164,28 @@ export class CanvasDiceRollComponent implements AfterViewInit {
     light.shadow.mapSize.height = 1024;
     this.scene.add(light);
 
+	// PARA CARGARLE UNA IMAGEN SOLA
+    //var texturePainting1 = new THREE.TextureLoader().load( "../assets/images/dashboard.jpg", );
+    
+	// PAR CARGARLE UNA IMAGEN Y REPETIRLA
+	var texturePainting1 = new THREE.TextureLoader().load( "../assets/images/grid.png", function( texture ) {
+				texture.wrapS = THREE.RepeatWrapping;
+				texture.wrapT = THREE.RepeatWrapping;
+				texture.repeat.set( 20, 20 );
+			} );
+      var image = texturePainting1.image;
 
   	// FLOOR
-  	var floorMaterial = new THREE.MeshPhongMaterial( { color: '#00aa00', side: THREE.DoubleSide } );
-  	var floorGeometry = new THREE.PlaneGeometry(30, 30, 10, 10);
+  	// var floorMaterial = new THREE.MeshPhongMaterial( { color: '#00aa00', side: THREE.DoubleSide } );
+    var floorMaterial = new THREE.MeshBasicMaterial( { color: 0xffffff, map: texturePainting1,side: THREE.DoubleSide } );
+  	var floorGeometry = new THREE.PlaneGeometry(60, 60, 10, 10);
   	var floor = new THREE.Mesh(floorGeometry, floorMaterial);
   	floor.receiveShadow  = true;
   	floor.rotation.x = Math.PI / 2;
   	this.scene.add(floor);
   	// SKYBOX/FOG
-  	var skyBoxGeometry = new THREE.CubeGeometry( 10000, 10000, 10000 );
+  	//var skyBoxGeometry = new THREE.CubeGeometry( 10000, 10000, 10000 );
+	 var skyBoxGeometry = new THREE.CubeGeometry( 35, 35, 35 );
   	var skyBoxMaterial = new THREE.MeshPhongMaterial( { color: 0x9999ff, side: THREE.BackSide } );
   	var skyBox = new THREE.Mesh( skyBoxGeometry, skyBoxMaterial );
   	//scene.add(skyBox);
